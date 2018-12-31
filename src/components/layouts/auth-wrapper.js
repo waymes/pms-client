b9d5c9dc20490113b977a93b5bc3a95324618e0f
werbi <vladimirbar7@gmail.com>
2018-12-31 12:44:49 +0200

    redirect fix

--- src/components/layouts/auth-wrapper.js
+++ src/components/layouts/auth-wrapper.js
@@ -2,33 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
-import { withRouter } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 
 import { getToken } from '../../store/selectors/auth';
 
 const authCheck = ({ withAuth }) => Component => compose(
-  connect(state => ({ isLoggedIn: !!getToken(state) })),
-  withRouter
+  connect(state => ({ isLoggedIn: !!getToken(state) }))
 )(
   class extends React.PureComponent {
     static propTypes = {
-      isLoggedIn: PropTypes.bool.isRequired,
-      history: PropTypes.shape({
-        push: PropTypes.func.isRequired
-      }).isRequired
-    }
-
-    componentDidUpdate() {
-      const { isLoggedIn, history } = this.props;
-
-      if (withAuth && !isLoggedIn) {
-        history.push('/login');
-      } else if (!withAuth && isLoggedIn) {
-        history.push('/profile');
-      }
+      isLoggedIn: PropTypes.bool.isRequired
     }
 
     render() {
+      const { isLoggedIn } = this.props;
+
+      if (withAuth && !isLoggedIn) {
+        return <Redirect to="/login" />
+      } else if (!withAuth && isLoggedIn) {
+        return <Redirect to="/profile" />
+      }
       return <Component {...this.props} />;
     }
   }

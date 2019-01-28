f2d82eebaa855860e8c58493242bb4a7df165754
emazwpmsji <vladimirbar7@gmail.com>
2019-01-28 21:06:24 +0200

    homepage two blocks

---
+++ src/components/routes/home/constants.js
@@ -0,0 +1,47 @@
+export const characteristics = [
+  {
+    icon: 'fas fa-cloud-download-alt',
+    title: 'Built for the Cloud',
+    description: 'Benefit from the scalability, performance and security of a native Cloud solution.'
+  },
+  {
+    icon: 'fas fa-user-cog',
+    title: 'Responsive design',
+    description: 'Created to be used from your PC, Mac, laptop, tablet or mobile via any modern browser.'
+  },
+  {
+    icon: 'fas fa-users',
+    title: 'Secure self service',
+    description: 'Connect your whole workforce, wherever they are, and save everyone time.'
+  },
+  {
+    icon: 'fas fa-check-circle',
+    title: 'Approval workflows',
+    description: 'Ensure processes keep moving smoothly through your approval cycles.'
+  },
+  {
+    icon: 'fas fa-bullhorn',
+    title: 'Notifications & Alerts',
+    description: 'Help everyone stay on top of key activities with emails and system inboxes.'
+  },
+  {
+    icon: 'fas fa-desktop',
+    title: 'Centralized reporting',
+    description: 'Benefit from a single source of data for faster, better reporting.'
+  },
+  {
+    icon: 'fas fa-sync-alt',
+    title: 'Configuration tools',
+    description: 'Flex the system to fit your processes and way of working.'
+  },
+  {
+    icon: 'fas fa-lock',
+    title: 'Role-based security',
+    description: 'Ensure employees and managers only see the information you give them permission to see.'
+  },
+  {
+    icon: 'fas fa-globe',
+    title: 'Global capabilities',
+    description: 'Take advantage of system-wide international capabilities, language translations and more.'
+  },
+]
--- src/components/routes/home/index.js
+++ src/components/routes/home/index.js
@@ -1,7 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => (
-  <div>
-    Homepage
-  </div>
-);
+import { characteristics } from './constants';
+import Button from '../../common/button';
+import './style.scss';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="homePage">
+        <div className="homePage__header">
+          <h1>Human Resources Management</h1>
+          <h2>System overview</h2>
+        </div>
+        <div className="homePage__description bg-white">
+          <div className="container">
+            <div className="row">
+              <div className="col-6">
+                <h5 className="text-danger">INTEGRATED HUMAN RESOURCES SOFTWARE</h5>
+                <h3 className="mb-4">Powerful features at your fingertips</h3>
+                <p className="mb-4">
+                  Designed for international organizations, PMS is a modern,
+                  configurable and growing HR software suite that combines extensive functionality
+                  for human resources management with the advantages of the latest Cloud technologies.
+                </p>
+                <p className="mb-4">
+                  By choosing PMS, you’ll benefit from a secure, scalable and cost-effective
+                  HR solution that is easy to manage, trusted worldwide, and regularly updated with
+                  new features, so you’ll never get left behind.
+                </p>
+                <Button>See all modules</Button>
+              </div>
+              <div className="col-6">photo</div>
+            </div>
+          </div>
+        </div>
+        <div className="homePage__chars container text-center">
+          <h5 className="text-danger mb-3">COMMON CHARACTERISTICS</h5>
+          <h3 className="mb-4">Proven HR solution with a modern approach</h3>
+          <p>
+            Whichever modules you select from PMS integrated suite, you’ll
+            benefit from centralized data, a consistent user-interface, time-saving automation,
+            advanced security and much more.
+          </p>
+          <div className="row">
+            {characteristics.map(char => (
+              <div className="col-12 col-sm-6 col-md-4">
+                <i className={char.icon} />
+                <h5>{char.title}</h5>
+                <p>{char.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
